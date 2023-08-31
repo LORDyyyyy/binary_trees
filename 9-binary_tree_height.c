a38463aca2c5fafe8b11f0bb279efb2564993047
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-01 01:39:34 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,22 +10,22 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 1, right_height = 1;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height += binary_tree_height(tree->left);
+	right_height += binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 	{
-		return (left_height + 1);
+		return (left_height);
 	}
 	else
 	{
-		return (right_height + 1);
+		return (right_height);
 	}
 }
