51a6f17a56d651e24987d2eb332af7cb12688612
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-01 02:15:07 +0300

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,30 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child.
- * @tree: pointer to the root node of the tree to count the number of nodes
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the nodes.
  *
- * Return: Number of nodes in the tree.
+ * Return: Number of nodes with at least 1 child in the tree.
  *
  * If tree is NULL, the function returns 0.
- * A NULL pointer is not a node
+ * A NULL pointer is not a node.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
-	if (!tree->parent)
-	{
-		nodes += 1;
-	}
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	nodes += binary_tree_leaves(tree->left);
-	nodes += binary_tree_leaves(tree->right);
+	if (tree->left || tree->right)
+	{
+		nodes += 1;
+	}
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
 
 	return (nodes);
 }
