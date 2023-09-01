c1025ed49e201298748aaaf8b7440b2d5d552fab
LORDyyyyy <mlord6271@gmail.com>
2023-09-01 04:53:43 +0300

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		if (!tree->left || !tree->right)
 			return (1);
 
-		return (binary_tree_is_perfect(tree->left)
+		return (binary_tree_is_perfect(tree->left) \
 				&& binary_tree_is_perfect(tree->right));
 	}
 	return (0);
